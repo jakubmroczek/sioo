a633f50dbb99afa58f17daf64725121d3a399fe1
Jakub Mroczek <jakub.mroczek@tomtom.com>
2021-04-04 00:07:16 +0200

    used boston school to implement the feaure

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
+# assume we look for miniums
+
+def get_function():
+    pass
+
+
+def get_function_range():
+    pass
+
+
+def is_function_unimodal_in_range(function, range):
+    pass
+
+
+def get_unimodal_range(function, range):
+    pass
+
+
+def run_function(function, range):
+    pass
+
+
+def visualize_result(result):
+    pass
+
+
 if __name__ == '__main__':
-    print('hello world')
+    function = get_function()
+    range = get_function_range()
+
+
+    if not is_function_unimodal_in_range(function, range):
+        range = get_unimodal_range(function, range)
+
+
+    result =  run_function(function, range)
+    visualize_result(result)
