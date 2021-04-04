8d2e5de227eada722484a8dfaef04e79b481beaa
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-04-04 17:38:15 +0200

    implemented the bisection method

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ class UnaryFunction:
 
     def evalute(self, x):
         self.x = x
-        print(eval(self.expression))
         return eval(self.expression)
 
 def get_function(expression):
@@ -24,41 +23,49 @@ def get_function(expression):
 
 class BisectionOptimzer(object):
 
-    def step(self, f, a, b):
-        if f.evalute(a)*f.evalute(b) >= 0:
+
+    def optimize(self, function, functionRange, stopCondition, epochs):
+        a, b = functionRange.low,  functionRange.high
+
+
+        if function.evalute(a) * function.evalute(b) >= 0:
             print("Bisection method fails.")
             return None
-
-        print(a, b)
 
         a_n = a
         b_n = b
 
-        m_n = (a_n + b_n)/2
-        f_m_n = f.evalute(m_n)
+        for epoch in range(epochs):
+            if stopCondition(epoch, a):
+                break
 
-        if f.evalute(a_n)*f_m_n < 0:
-            a_n = a_n
-            b_n = m_n
-        elif f.evalute(b_n)*f_m_n < 0:
-            a_n = m_n
-            b_n = b_n
-        elif f_m_n == 0:
-            print("Found exact solution.")
-            return m_n
-        else:
-            print("Bisection method fails.")
-            return None
+            m_n = (a_n + b_n)/2
+            f_m_n = function.evalute(m_n)
+
+            if function.evalute(a_n)*f_m_n < 0:
+                a_n = a_n
+                b_n = m_n
+            elif function.evalute(b_n)*f_m_n < 0:
+                a_n = m_n
+                b_n = b_n
+            elif f_m_n == 0:
+                print("Found exact solution.")
+                return m_n
+            else:
+                print("Bisection method fails.")
+                return None
 
         return (a_n + b_n)/2
 
 
 
 class GoldenDivisionOptimizer(object):
-    pass
+
+    def optimize(self):
+        pass
 
 
-class Range:
+class FunctionRange:
     def __init__(self, low, high):
         super().__init__()
         self.low = low
@@ -80,19 +87,8 @@ def get_unimodal_range(function, range):
     pass
 
 
-def optimize(optimizer, function, range, stopCondition):
-    result_x = range.low
-
-    epoch = 0
-
-    while not stopCondition(epoch, result_x):
-        result_x = optimizer.step(function, range.low, range.high)
-        epoch = epoch + 1
-
-    return result_x
-
 def visualize_result(result):
-    pass
+    print(f'The result is {result}')
 
 
 class ProgramArguments:
@@ -101,8 +97,9 @@ class ProgramArguments:
         super().__init__()
         self.optimizerType = OptimizerType.BISECTION
         self.expression = 'x ** 2 - 1'
-        self.range = Range(0, 10)
+        self.functionRange = FunctionRange(0, 10)
         self.stopCondition = lambda epoch, result :  False
+        self.epochs = 25
 
 if __name__ == '__main__':
     arguments = ProgramArguments()
@@ -110,11 +107,12 @@ if __name__ == '__main__':
     # cli arguments layer
     optimizer = get_optimizer(arguments.optimizerType)
     function = get_function(arguments.expression)
-    range = arguments.range
+    functionRange = arguments.functionRange
     stopCondition = arguments.stopCondition
+    epochs = arguments.epochs
 
     if not is_function_unimodal_in_range(function, range):
         range = get_unimodal_range(function, range)
 
-    result =  optimize(optimizer, function, range, stopCondition)
-    visualize_result(result)
+    result_x =  optimizer.optimize(function, functionRange, stopCondition, epochs)
+    visualize_result(result_x)
