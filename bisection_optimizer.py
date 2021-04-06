f9aaa0f1b4036e3b16b6e112a435e045a61a6912
Jakub Mroczek <jakub.mroczek@tomtom.com>
2021-04-07 00:40:41 +0200

    implemented simple caching in the custom bisection

--- bisection_optimizer.py
+++ bisection_optimizer.py
@@ -2,29 +2,36 @@ from function import FunctionInterval
 
 class BisectionOptimizer(object):
     def optimize(self, function, functionInterval, stopCondition, epochs):
-        a, b = functionInterval.low,  functionInterval.high
+        a_n, b_n = functionInterval.low,  functionInterval.high
+        f_a_n, f_b_n, f_m_n = None, None, None
+        intermediate_intervals = [FunctionInterval(a_n, b_n)]
 
-        if function.evalute(a) * function.evalute(b) >= 0:
+        if function.evalute(a_n) * function.evalute(b_n) >= 0:
             print("Bisection method fails.")
             return None
-
-        a_n = a
-        b_n = b
-        intermediate_intervals = [FunctionInterval(a_n, b_n)]
 
         for epoch in range(epochs):
             if stopCondition(epoch, a_n, b_n):
                 break
 
-            m_n = (a_n + b_n)/2
+            m_n = (a_n + b_n) / 2
             f_m_n = function.evalute(m_n)
+            f_a_n = f_a_n if f_a_n != None else function.evalute(a_n)
 
-            if function.evalute(a_n)*f_m_n < 0:
+            if f_a_n * f_m_n < 0:
                 a_n = a_n
                 b_n = m_n
+                # Clearing cache
+                f_b_n = None
                 intermediate_intervals.append(FunctionInterval(a_n, b_n))
-            elif function.evalute(b_n)*f_m_n < 0:
+                continue
+
+            f_b_n = f_b_n if f_b_n != None else function.evalute(b_n)
+
+            if f_b_n * f_m_n < 0:
                 a_n = m_n
+                # Clearing cache
+                f_a_n = None
                 b_n = b_n
                 intermediate_intervals.append(FunctionInterval(a_n, b_n))
             elif f_m_n == 0:
