deb11a5ce6124cd5b07e2d517d48effd158b764a
Jakub Mroczek <jakub.mroczek@tomtom.com>
2021-04-06 22:24:53 +0200

    printing to console the calculatino result

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ def get_unimodal_range(function, functionInterval, n):
     return unimodal_range
 
 class CalculationResult:
-
     def __init__(self, function, user_interval, unimodal_interval, minimum_interval, intermediate_intervals):
         super().__init__()
         self.function = function
@@ -34,6 +33,17 @@ class CalculationResult:
         self.minimum_end_interval = FunctionInterval(minimum_interval[0], minimum_interval[1])
         self.intermediate_intervals = intermediate_intervals
 
+    def __str__(self) -> str:
+        result = ''
+        result += f'Function: "{self.function.expression}"\n'
+        result += f'User interval: "{self.user_interval}"\n'
+        result += f'Unimodal interval: "{self.unimodal_interval}"\n'
+        result += f'Minimum interval: "{self.minimum_end_interval}"\n'
+        result += f'Intermediate intervals: "{self.intermediate_intervals}"'
+        return result
+
+def log_to_console(obj):
+    print(obj)
 
 def calculate(arguments: ProgramArguments):
     optimizer = get_optimizer(arguments.optimizerType)
@@ -52,7 +62,9 @@ def calculate(arguments: ProgramArguments):
     result_x, minimum_end_interval, intermediate_intervals =  optimizer.optimize(function, unimodal_interval,
                                                                                  stopCondition, epochs)
 
-    return CalculationResult(function, user_function_interval, unimodal_interval, minimum_end_interval, intermediate_intervals)
+    calculationResult = CalculationResult(function, user_function_interval, unimodal_interval, minimum_end_interval, intermediate_intervals)
+    log_to_console(calculationResult)
+    return calculationResult
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
