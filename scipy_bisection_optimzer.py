75ffa9fd019ade378cd3f2dc48c628824dec43e7
Jakub Mroczek <jakub.mroczek@tomtom.com>
2021-04-07 00:05:23 +0200

    scipy algorithm takes both the max iteration limit and xtol

--- scipy_bisection_optimzer.py
+++ scipy_bisection_optimzer.py
@@ -1,6 +1,7 @@
 from scipy import optimize
 
 class SciPyBisectionOptimizer(object):
-    def optimize(self, function, functionInterval):
+    def optimize(self, function, functionInterval, xtol, max_iterations):
         functionWrapper = lambda x : function.evalute(x)
-        return optimize.bisect(functionWrapper, functionInterval.low, functionInterval.high)
+        return optimize.bisect(functionWrapper, functionInterval.low, functionInterval.high, xtol=xtol,
+                               maxiter=max_iterations)
