f02c817db025b26f662de6301be47098489ceba4
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-04-07 10:05:59 +0200

    rewrtoe the golden ratio method so that it is like in the lectures

--- golden_section_search_optimizer.py
+++ golden_section_search_optimizer.py
@@ -3,28 +3,34 @@ from math import sqrt
 
 class GoldenSectionSearchOptimizer(object):
     def optimize(self, function, functionInterval, stopCondition, epochs):
+        golden_ratio = 0.61803
+
         a, b  = functionInterval.low, functionInterval.high
         intermediate_intervals = [FunctionInterval(a, b)]
 
-        gr = (sqrt(5) + 1) / 2
-
-        c = b - (b - a) / gr
-        d = a + (b - a) / gr
+        x1 = (b - a) * (-golden_ratio) + b
+        x2 = (b - a) * golden_ratio + a
 
         for epoch in range(epochs):
             if stopCondition(epoch, a, b):
                 break
 
-            if function.evalute(c) < function.evalute(d):
-                b = d
+            f_x1 = function.evalute(x1)
+            f_x2 = function.evalute(x2)
+
+            if f_x1 > f_x2:
+                a = x1
+                x1 = x2
+                x2 = (b - a) * golden_ratio + a
+                # Dumb, but I like how it corresponds with lectures
+                b = b
             else:
-                a = c
+                # Dumb, but I like how it corresponds with lectures
+                a = a
+                b = x2
+                x2 = x1
+                x1 = (b - a) * (-golden_ratio) + b
 
             intermediate_intervals.append(FunctionInterval(a, b))
-
-            # Recompute both c and d here to avoid loss of precision which may lead to incorrect results or infinite
-            # loop
-            c = b - (b - a) / gr
-            d = a + (b - a) / gr
 
         return (b + a) / 2, (a, b), intermediate_intervals
