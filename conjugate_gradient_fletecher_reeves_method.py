1000a2477e7455f59505cfc16baea9ee8471d8e2
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-04-19 12:09:17 +0200

    introducig architecture to preocess the fletecher reevers

--- conjugate_gradient_fletecher_reeves_method.py
+++ conjugate_gradient_fletecher_reeves_method.py
@@ -1,6 +1,6 @@
 from function import MultiNumberFunction
 
 class ConjugateGradientFletcherReevesMethod:
-    def evaluate(self, function : MultiNumberFunction, start_position):
+    def optimize(self, function : MultiNumberFunction, start_position):
         print('Hello world')
         return start_position
---
+++ multi_dimensional_calculation.py
@@ -0,0 +1,5 @@
+from conjugate_gradient_fletecher_reeves_method import ConjugateGradientFletcherReevesMethod
+
+def multidimensional_calculation(arguments):
+    method = ConjugateGradientFletcherReevesMethod()
+    return method.optimize()
