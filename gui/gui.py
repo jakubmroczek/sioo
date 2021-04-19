448f191bb9e42d13229867e6c0c134f1c05d86ad
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-04-19 10:45:52 +0200

    removing only not basic widgest

--- gui/gui.py
+++ gui/gui.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog, QLabel, QMessageB
 class GUI(QDialog):
 
     MAX_NUMBER_OF_FUNCTION_ARGUMENTS = 8
+    NUMBER_OF_BASIC_WIDGETS = 2
 
     def __init__(self, parent=None):
         super(GUI, self).__init__(parent)
@@ -13,22 +14,24 @@ class GUI(QDialog):
         self.originalPalette = QApplication.palette()
         self.setWindowTitle("SIOO")
 
+        self.layout = QGridLayout()
+
+        self._add_basic_widgets()
+
+        self.setLayout(self.layout)
+
+    def setOnCalculationStart(self, callback):
+        self.onCalculationStartCallback = callback
+
+    def _add_basic_widgets(self):
         # Number of function arguments
         functionArgumentLabel = QLabel('Number of functions argument:')
         self.functionArgumentNumberComboBox = QComboBox()
         self.functionArgumentNumberComboBox.addItems([str(x) for x in range(1, self.MAX_NUMBER_OF_FUNCTION_ARGUMENTS
                                                                             + 1)])
         self.functionArgumentNumberComboBox.currentTextChanged.connect(self._on_function_arguments_number_changed)
-
-        self.layout = QGridLayout()
-
         self.layout.addWidget(functionArgumentLabel, 0, 0, 1, 1)
         self.layout.addWidget(self.functionArgumentNumberComboBox, 0, 1, 1, 1)
-
-        self.setLayout(self.layout)
-
-    def setOnCalculationStart(self, callback):
-        self.onCalculationStartCallback = callback
 
     def _onCalculationStart(self):
         # Clean the plot
@@ -49,10 +52,13 @@ class GUI(QDialog):
 
 
     def _on_function_arguments_number_changed(self, value):
-        self._remove_all_widgets()
+        self._remove_extra_widgets()
 
-    def _remove_all_widgets(self):
-        for i in reversed(range(self.layout.count())):
+    def _remove_extra_widgets(self):
+        '''
+        Removes all the widget except the basic one (function arguments numbr label and combo box).
+        '''
+        for i in reversed(range(self.layout.count() - self.NUMBER_OF_BASIC_WIDGETS)):
             self.layout.itemAt(i).widget().setParent(None)
 
 
