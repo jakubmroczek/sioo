9d57654fcb0936499f2cd60ca22d7bf51eff370f
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-04-19 12:03:32 +0200

    fletcher reeves stub file

---
+++ conjugate_gradient_fletecher_reeves_method.py
@@ -0,0 +1,6 @@
+from function import MultiNumberFunction
+
+class ConjugateGradientFletcherReevesMethod:
+    def evaluate(self, function : MultiNumberFunction, start_position):
+        print('Hello world')
+        return start_position
