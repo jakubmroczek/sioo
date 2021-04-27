3493931ff124fa699bafc243c6b4566c0b988ff3
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-04-27 22:43:15 +0200

    passing derivatives in the to the conjugate gradient algorithms

--- multi_dimensional_calculation.py
+++ multi_dimensional_calculation.py
@@ -7,12 +7,20 @@ def multidimensional_calculation(arguments):
     method = ConjugateGradientFletcherReevesMethod(goldenSectionSearchOptimzier)
 
     function = _get_function(arguments.expression, arguments.argc)
+    derivatives = _get_derivatives(arguments.derivatives_expressions, arguments.argc)
     x = arguments.start_x
     epsilon = arguments.epsilon
     alpha = arguments.alpha
     max_iterations = arguments.max_iterations
 
-    return method.optimize(function, x, epsilon, alpha, max_iterations)
+    return method.optimize(function, x, epsilon, alpha, max_iterations, derivatives)
 
 def _get_function(expression, argc):
     return MultiNumberFunction(expression, argc)
+
+def _get_derivatives(derivatives_expressions, argc):
+    derivative_functions = []
+    for expression in derivatives_expressions:
+        function = _get_function(expression, argc)
+        derivative_functions.append(function)
+    return derivative_functions
