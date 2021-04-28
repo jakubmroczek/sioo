c948e8d57585efa1285485d866ac94761c7c9b05
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-04-28 11:49:29 +0200

    added matplot lib 3d example on click

--- gui/plot_3d.py
+++ gui/plot_3d.py
@@ -5,8 +5,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Plot3D:
+    def f(self, x, y):
+        return np.sin(np.sqrt(x ** 2 + y ** 2))
+
     def show(self):
+        x = np.linspace(-6, 6, 30)
+        y = np.linspace(-6, 6, 30)
+
+        X, Y = np.meshgrid(x, y)
+        Z = self.f(X, Y)
+
         fig = plt.figure()
         ax = plt.axes(projection='3d')
+        ax.contour3D(X, Y, Z, 50, cmap='binary')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z');
 
         plt.show()
