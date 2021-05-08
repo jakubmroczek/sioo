9b52044e2bbb06a203ce6ad3bfa8b5e28335a5a1
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-05-09 00:18:20 +0200

    bieda wizualizacje, nie działają bardziej zaawansowane funkcje matematyczne

--- gui/plot_3d.py
+++ gui/plot_3d.py
@@ -1,14 +1,19 @@
-from mpl_toolkits import mplot3d
+# from mpl_toolkits import mplot3d
 
-# %matplotlib inline
 import numpy as np
 import matplotlib.pyplot as plt
 
 class Plot3D:
-    def f(self, x, y, function):
-        Z = np.sin(np.sqrt(x ** 2 + y ** 2))
-        print(Z)
-        return Z
+    def f(self, x, y, function):        
+        Z = []
+        for a, b in zip(x, y):
+            local = []
+            for c, d in zip(a, b):
+                local.append(function.evaluate([c, d]))
+            Z.append(local)
+        # Z = np.sin(np.sqrt(x ** 2 + y ** 2))
+        return np.array(Z)
+
     # def f(self, X, Y, function):
     #     z = np.array([[1, 2], [2, 2], [3, 4]])
     #     # TODO: Poor cartesian product xD
@@ -19,13 +24,14 @@ class Plot3D:
     #     return z
 
     def show(self, result):
-
         #TODO: this must be adujsted to initial serach point
         x = np.linspace(-6, 6, 30)
         y = np.linspace(-6, 6, 30)
-
+        
         X, Y = np.meshgrid(x, y)
+
         Z = self.f(X, Y, result.function)
+        # print(Z.shape)
 
         fig = plt.figure()
         ax = plt.axes(projection='3d')
