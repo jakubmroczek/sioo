0d2a41618a0835fff878d25fa10e8988f25ef226
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-05-10 11:11:19 +0200

    marking the intermediate steps

--- gui/plot_3d.py
+++ gui/plot_3d.py
@@ -1,42 +1,42 @@
-# from mpl_toolkits import mplot3d
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 class Plot3D:
-    def f(self, x, y, function):        
-        Z = []
-        for a, b in zip(x, y):
-            local = []
-            for c, d in zip(a, b):
-                local.append(function.evaluate([c, d]))
-            Z.append(local)
-        # Z = np.sin(np.sqrt(x ** 2 + y ** 2))
-        return np.array(Z)
-
-    # def f(self, X, Y, function):
-    #     z = np.array([[1, 2], [2, 2], [3, 4]])
-    #     # TODO: Poor cartesian product xD
-    #     for x in X:
-    #         for y in Y:
-    #             np.append(z, function.evaluate((x, y)))
-    #     print(z)
-    #     return z
-
     def show(self, result):
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        
+        self._plot_function_surface(result, ax)
+        self._plot_intermediate_steps(result, ax)
+        
+        # TODO: Rename the title
+        ax.set_title('surface');
+        
+        plt.show()
+
+    def _plot_function_surface(self, result, ax):
         #TODO: this must be adujsted to initial serach point
         x = np.linspace(-6, 6, 30)
         y = np.linspace(-6, 6, 30)
         
         X, Y = np.meshgrid(x, y)
 
-        Z = self.f(X, Y, result.function)
-        # print(Z.shape)
-
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
+        Z = self._function_at_xy(X, Y, result.function)
+        
         ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
                 cmap='viridis', edgecolor='none')
-        ax.set_title('surface');
+        
+    def _plot_intermediate_steps(self, result, ax):
+        xs = [1,  2 , 43, 4]
+        ys = [1,  2 , 43, 4]
+        zs = [1,  2 , 43, 4]
+        ax.scatter(xs, ys, zs, marker='x')
 
-        plt.show()
+    def _function_at_xy(self, x, y, function):        
+            Z = []
+            for a, b in zip(x, y):
+                local = []
+                for c, d in zip(a, b):
+                    local.append(function.evaluate([c, d]))
+                Z.append(local)
+            return np.array(Z)    
