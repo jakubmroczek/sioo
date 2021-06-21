ed6152fa9d7a8d55aaca982df2926de86243ef7e
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-06-21 16:47:59 +0200

    initial implementatio of the sumt

--- sumt.py
+++ sumt.py
@@ -2,20 +2,42 @@ from function import PenaltyMethodFunction
 
 
 class SUMT:
-    def __init__(self, fletcher_reves, growth_param):
+    def __init__(self, fletcher_reves, growth_param, epsilon):
         self.fletcher_reves = fletcher_reves
         self.growth_param = growth_param
+        self.epsilon = epsilon
         # TODO: Check that x0 avoids at least one constraint
 
-    def optimize(self, function : PenaltyMethodFunction, x_0, epsilon, c_0, derivatives):
+    def optimize(self, function : PenaltyMethodFunction, derivatives, x_0, c_0):
         # Init step
         assert function.avoids_any_constraint(x_0)
         
         max_iter = 1_000_000 
-        c = c_0
+        c_k = c_0
+        x_k_1 = x_0
+        x_k = x_k_1
 
         for k in range(1, max_iter):
             # TODO: set c0 of the constarained function
-            function.set_penalty_parameter(c)
-            optimum = self.fletcher_reves.optimize('some args')
-            
+         
+            # TODO: Pass additional params
+            x_k = self._unconstrained_search()
+
+            if self._should_stop(function, x_k_1, x_k):
+                return x_k
+
+            c_k = self.growth_param * c_k
+
+            function.set_penalty_parameter(c_k)
+
+    def _unconstrained_search(self, params):
+        return self.fletcher_reves.optimize('some args')
+
+    def _should_stop(self, function, x_k_1, x_k):
+        if abs(x_k_1 - x_k) < self.epsilon:
+            return True
+
+        if abs(function.evaluate(x_k_1) - function.evaluate(x_k)) < self.epsilon:
+            return True
+
+        return False
