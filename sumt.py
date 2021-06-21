ec57f2fcb1e797cf139c2fbd1f352d7de526fc37
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-06-21 17:11:17 +0200

    more implementatoin

--- sumt.py
+++ sumt.py
@@ -1,5 +1,5 @@
 from function import PenaltyMethodFunction
-
+from math_utils.vector import vector_length
 
 class SUMT:
     def __init__(self, fletcher_reves, growth_param, epsilon):
@@ -17,9 +17,9 @@ class SUMT:
         x_k = x_k_prev  
 
         for k in range(1, max_iter):
-            x_k = self._unconstrained_search(function, x_k_prev, derivatives)
+            x_k, _ = self._unconstrained_search(function, x_k_prev, derivatives)
 
-            if self._should_stop(function, x_k, x_k_prev):
+            if self._has_converged(function, x_k_prev, x_k):
                 return x_k
 
             c_k = self.growth_param * c_k
@@ -32,11 +32,15 @@ class SUMT:
         epsilon = self.epsilon
         return self.fletcher_reves.optimize(function, x_1, epsilon, alpha, n, derivatives)
 
-    def _should_stop(self, function, x_k_1, x_k):
-        if abs(x_k_1 - x_k) < self.epsilon:
+    def _has_converged(self, function, x_k_prev, x_k):
+        print('HAS CONVERGED')
+        print(x_k)
+        print(x_k_prev)
+        
+        if abs(vector_length(x_k) - vector_length(x_k_prev)) < self.epsilon:
             return True
 
-        if abs(function.evaluate(x_k_1) - function.evaluate(x_k)) < self.epsilon:
+        if abs(function.evaluate(x_k) - function.evaluate(x_k_prev)) < self.epsilon:
             return True
 
         return False
